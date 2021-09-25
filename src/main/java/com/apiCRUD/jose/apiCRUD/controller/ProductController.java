3b78ed7a632f928d6a952bf4d561652d814571ef
jlizarragavil <joselizarragavil@gmail.com>
2021-09-25 12:53:11 +0200

    update product

--- src/main/java/com/apiCRUD/jose/apiCRUD/controller/ProductController.java
+++ src/main/java/com/apiCRUD/jose/apiCRUD/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -67,5 +68,21 @@ public class ProductController {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 			
 		}
+	}
+	
+	@PutMapping("/products/{id}")
+	public ResponseEntity <Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product){
+		 	Optional <Product> productData = productRepository.findById(id);
+		 	if(productData.isPresent()) {
+		 		Product updatedProduct = productData.get();
+		 		updatedProduct.setName(product.getName());
+		 		updatedProduct.setOwner(product.getOwner());
+		 		updatedProduct.setPrice(product.getPrice());
+		 		
+		 		return new ResponseEntity<>(productRepository.save(updatedProduct), HttpStatus.OK);
+
+		 	}else {
+		 		 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		 	}
 	}
 }
